b828ce5be4985942e53a13bfdf23b2ee2df3f7ca
Srujan <srujandivakar07@gmail.com>
2025-08-28 21:49:12 +0530

    day2

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import datetime
 
 # Video Path
 VIDEO_CONFIG = {
-	"VIDEO_CAP" : "/Users/levi/Videos/video:7.mp4",
+	"VIDEO_CAP" : "/Users/levi/Videos/7.mp4",
 	"IS_CAM" : False,
 	"CAM_APPROX_FPS": 3,
 	"HIGH_CAM": True,
